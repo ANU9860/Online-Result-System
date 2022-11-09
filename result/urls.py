c92cf702c3ab9605893cbc4f1c818eda6156a70d
anu9860 <anushkasingh.mail333@gmail.com>
2022-11-09 12:22:54 -0800

    first commit

---
+++ result/urls.py
@@ -0,0 +1,15 @@
+"""result URL Configuration"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+admin.site.site_header = "Online Result System"
+admin.site.site_title = "Online Result System"
+admin.site.index_title = "Online Result Site"
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('faculty/', include('faculty.urls')),
+    path('student/', include('student.urls')),
+    path('admin/', admin.site.urls),
+]
